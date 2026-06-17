8420078759959b645ef1f94775dd63e21d11b103
Shreya Meshram <shreya@gmail.com>
2026-06-17 13:51:47 +0530

    Stronger mobile scroll lock using documentElement

--- src/App.jsx
+++ src/App.jsx
@@ -37,11 +37,14 @@ const TopNav = () => {
   React.useEffect(() => {
     if (mobileMenuOpen) {
       document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = 'unset';
+      document.documentElement.style.overflow = 'unset';
     }
     return () => {
       document.body.style.overflow = 'unset';
+      document.documentElement.style.overflow = 'unset';
     };
   }, [mobileMenuOpen]);
 
